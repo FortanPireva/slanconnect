8dcf5b081616a45579f475dc0fe9877b67a8f166
Fortan Pireva <fortanpireva99@gmail.com>
2022-01-09 23:24:13 +0100

    finished useAuth hook

--- src/contexts/useAuth.jsx
+++ src/contexts/useAuth.jsx
@@ -1,4 +1,11 @@
-import { Children, createContext, useEffect, useState } from "react";
+import {
+  Children,
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import authService from "../services/Auth/auth";
 import { useNavigate } from "react-router-dom";
 const AuthContext = createContext({});
@@ -36,4 +43,49 @@ export function AuthProvider({ children }) {
       .catch((error) => setError(error))
       .finally(() => setLoading(false));
   }
+  function signup(email, password) {
+    setLoading(true);
+    authService
+      .signup(email, password)
+      .then((user) => {
+        setUser(user);
+        navigate("/");
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => setLoading(false));
+  }
+  function logout() {
+    authService.logout().then(() => setUser(undefined));
+  }
+
+  // Make the provider update only when it should.
+  // We only want to force re-renders if the user,
+  // loading or error states change.
+  //
+  // Whenever the `value` passed into a provider changes,
+  // the whole tree under the provider re-renders, and
+  // that can be very costly! Even in this case, where
+  // you only get re-renders when logging in and out
+  // we want to keep things very performant.
+  const memoedValue = useMemo(
+    () => ({
+      user,
+      loading,
+      error,
+      login,
+      logout,
+      signUp,
+    }),
+    [user, loading, error]
+  );
+  return (
+    <AuthContext.Provider value={memoedValue}>
+      {!loadingInitial && children}
+    </AuthContext.Provider>
+  );
+}
+export default function useAuth() {
+  return useContext(AuthContext);
 }
